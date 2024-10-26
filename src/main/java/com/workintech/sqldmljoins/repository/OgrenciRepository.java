7b11ac47c98f6ca652b92fc1efbf03080bdfeea0
MüminErsahin <75380505+MuminErsahin@users.noreply.github.com>
2024-10-27 01:29:03 +0300

    bitti

--- src/main/java/com/workintech/sqldmljoins/repository/OgrenciRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/OgrenciRepository.java
@@ -3,25 +3,30 @@ package com.workintech.sqldmljoins.repository;
 import com.workintech.sqldmljoins.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
 
 
     //Kitap alan öğrencilerin öğrenci bilgilerini listeleyin..
-    String QUESTION_2 = "SELECT * FROM  ogrenci WHERE ogrno IN(SELECT ogrno FROM islem)";
+    String QUESTION_2 = "SELECT ogrenci.*" +
+            "FROM ogrenci\n" +
+            "INNER JOIN islem ON ogrenci.ogrno = islem.ogrno;";
     @Query(value = QUESTION_2, nativeQuery = true)
     List<Ogrenci> findStudentsWithBook();
 
 
     //Kitap almayan öğrencileri listeleyin.
-    String QUESTION_3 = "SELECT * FROM  ogrenci WHERE ogrno NOT IN(SELECT ogrno FROM islem)";
+    String QUESTION_3 = "SELECT * FROM ogrenci WHERE ogrno NOT IN (SELECT ogrno FROM islem)";
     @Query(value = QUESTION_3, nativeQuery = true)
     List<Ogrenci> findStudentsWithNoBook();
 
     //10A veya 10B sınıfındaki öğrencileri sınıf ve okuduğu kitap sayısını getirin.
-    String QUESTION_4 = "SELECT o.sinif,COUNT(i.kitapno) FROM ogrenci o JOIN islem i ON o.ogrno = i.ogrno WHERE o.sinif = '10A' OR o.sinif = '10B' GROUP BY o.sinif";
+    String QUESTION_4 = "SELECT o.sinif, COUNT(i.islemno) FROM ogrenci AS o INNER JOIN islem AS i ON o.ogrno=i.ogrno\n" +
+            "WHERE sinif='10A' OR sinif='10B' \n" +
+            "GROUP BY o.sinif";
     @Query(value = QUESTION_4, nativeQuery = true)
     List<KitapCount> findClassesWithBookCount();
 
@@ -37,16 +42,17 @@ public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
 
     //--İsme göre öğrenci sayılarının adedini bulunuz.
     //--Ali: 2, Mehmet: 3
-    String QUESTION_7 = "SELECT ad,COUNT(ad) FROM ogrenci GROUP BY ad";
+    String QUESTION_7 = "SELECT ad, COUNT(ogrno) FROM ogrenci GROUP BY ad";
     @Query(value = QUESTION_7, nativeQuery = true)
     List<StudentNameCount> findStudentNameCount();
 
 
-    String QUESTION_8 = "SELECT sinif,COUNT(ogrno) FROM ogrenci GROUP BY sinif";
+    String QUESTION_8 = "SELECT sinif, COUNT(ogrno) FROM ogrenci GROUP BY sinif";
     @Query(value = QUESTION_8, nativeQuery = true)
     List<StudentClassCount> findStudentClassCount();
 
-    String QUESTION_9 = "SELECT o.ad,o.soyad,COUNT(i.kitapno) FROM ogrenci o JOIN islem i ON o.ogrno = i.ogrno GROUP BY ad,soyad";
+    String QUESTION_9 = "SELECT o.ad, o.soyad, COUNT(i.kitapno) FROM ogrenci As o \n" +
+            "INNER JOIN islem AS i ON o.ogrno=i.ogrno GROUP BY o.ad, o.soyad";
     @Query(value = QUESTION_9, nativeQuery = true)
     List<StudentNameSurnameCount> findStudentNameSurnameCount();
 }
